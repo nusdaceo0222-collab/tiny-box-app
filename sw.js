b778fe40aaab41f16a610a1cef1e5458b933111b
nusdaceo0222-collab <nusdaceo0222@gmail.com>
2026-01-14 12:21:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,29 @@
-const CACHE_NAME = "tinybox-v1";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "tinybox-v2";
+
+// GitHub Pages 프로젝트 페이지 경로
+const BASE = "/tiny-box-app/";
+
+const ASSETS = [
+  BASE,
+  BASE + "index.html",
+  BASE + "manifest.webmanifest",
+  BASE + "icon-192.png",
+  BASE + "icon-512.png",
+  BASE + "sw.js",
+];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener("fetch", (event) => {
@@ -10,3 +31,4 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
+;
