f3fa0c23a59a6c4415827dbc04a717a462743122
nusdaceo0222-collab <nusdaceo0222@gmail.com>
2026-01-14 11:37:00 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "tinybox-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
